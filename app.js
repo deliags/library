91b4a25a86681224c4d9e1fb29a8a30b7e1eedbd
deliags <deliagsmith@outlook.com>
2022-03-08 20:16:45 +0000

    safe commit

--- app.js
+++ app.js
@@ -28,20 +28,21 @@ function Book(name, author, pages, read) {
 // Adds new info function
 Book.prototype.info = function () {
   !this.read ? read = "not read" : read = "read";
-  return `${this.name} 
-          ${this.author}
-          ${this.pages} 
-          ${read}`;
+  return `Title: ${this.name}
+          \nAuthor: ${this.author}
+          \nPages: ${this.pages}
+          \nStatus: ${read}`;
 }
 
 //Input Book
 const newBook = new Book(bookTitle, bookAuthor, bookPages, isRead());
 
 //Adds the input book to the library
-function addBookToLibrary (book) {
+function addBookToLibrary(book) {
   library.push(book);
   inputCard.classList.remove('card-active')
   inputCard.classList.add('card-inactive');
+  showBookCard();
   return "Book added"
 }
 
@@ -50,17 +51,17 @@ const createNewBook = () => {
   inputCard.classList.remove('card-inactive')
   inputCard.classList.add('card-active');
   resetInput();
-  toggleSwitchLabel(); 
+  toggleSwitchLabel();
 };
 
 
 const toggleSwitchLabel = () => {
-  
+
   if (readSwitch.checked === true) {
     readLabel.textContent = "READ";
     inputForm.appendChild(readLabel)
-    
-  } else if(!readSwitch.checked) {
+
+  } else if (!readSwitch.checked) {
     readLabel.textContent = "NOT READ"
     inputForm.appendChild(readLabel);
   }
@@ -73,14 +74,33 @@ const resetInput = () => {
   //something for the switch
 }
 
+const resetBook = () => {
+  newBook.name = '';
+  newBook.author = '';
+  newBook.pages = '';
+  newBook.read = '';
+}
 
-// library.forEach(book => {
-//   // const card = document.createElement('div');
-//   // card.classList.add("card");
-//   // card.textContent = book.info();
-//   // main.appendChild(card);
 
-// })
+const showBookCard = () => {
+  library.forEach(book => {
+    const card = document.createElement('div');
+    card.classList.add("card");
+    // card.textContent = book.info();
+    const title = document.createElement('p')
+    const author = document.createElement('p')
+    const pages = document.createElement('p')
+
+    title.textContent = newBook.name
+    author.textContent = newBook.author
+    pages.textContent = `${newBook.pages} pages`
+    !newBook.read ? read = "not read" : read = "read";
+    read.textContent = read;
+
+    card.append(title, author, pages, read)
+    main.appendChild(card);
+  });
+}
 
 
 //Event Listeners
