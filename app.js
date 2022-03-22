4d1947be5b662087b9d572019574626abbbdb4ae
deliags <deliagsmith@outlook.com>
2022-03-22 17:34:04 +0000

    add class logic and clean scope

--- app.js
+++ app.js
@@ -1,150 +1,180 @@
-const main = document.querySelector("#main");
-const addButton = document.querySelector(".add-book");
-const inputCard = document.querySelector('#input-card');
-const inputForm = document.querySelector('.form');
-const readSwitch = document.querySelector('#switch-toggle');
-const readLabel = document.querySelector('.read-label');
-const submitButton = document.querySelector('.submit');
-const bookGrid = document.querySelector('#books-grid');
-const mainLabel = document.querySelector('.switch');
-const switchContainer = document.querySelector('.switch-container');
+const Library = (() => {
+  const addButton = document.querySelector(".add-book");
+  const submitButton = document.querySelector('.submit');
+  const bookGrid = document.querySelector('#books-grid');
 
+  let bookId = 0;
+  let library = [];
 
-let bookId = 0;
-let library = [];
-
-//Constructor Book Object
-function Book(name, author, pages, read, id) {
-  this.name = name;
-  this.author = author;
-  this.pages = pages;
-  this.read = read;
-  this.id = id;
-};
-
-//Adds a input card for the user to write info
-const createInputCard = () => {
-  inputCard.classList.remove('card-inactive')
-  inputCard.classList.add('card-active');
-  bookGrid.classList.add('grid-blurred');
-  resetInput();
-  toggleSwitchLabel();
-};
-
-const resetInput = () => {
-  inputForm.reset();
-};
-
-const toggleSwitchLabel = () => {
-
-  if (readSwitch.checked === true) {
-    readLabel.textContent = "READ";
-
-  } else if (!readSwitch.checked) {
-    readLabel.textContent = "NOT READ"
-  }
-  switchContainer.insertBefore(readLabel, mainLabel);
-  readSwitch.addEventListener('click', toggleSwitchLabel);
-};
-
-const removeInputCard = () => {
-  inputCard.classList.add('card-inactive');
-  inputCard.classList.remove('card-active');
-  bookGrid.classList.remove('grid-blurred');
-};
-
-const getUserInput = () => {
-  const bookTitle = document.querySelector('#book-name').value;
-  const bookAuthor = document.querySelector('#book-author').value;
-  const bookPages = document.querySelector('#book-pages').value;
-
-  //Saves the "read" status of a book entry
-  const isRead = readSwitch.onchange = () => {
-    let switchValue = readSwitch.checked;
-    return switchValue;
+  //Constructor Book Class
+  class Book {
+    constructor(name, author, pages, read, id) {
+      this.name = name;
+      this.author = author;
+      this.pages = pages;
+      this.read = read;
+      this.id = id;
+    }
   };
 
-  return new Book(bookTitle, bookAuthor, bookPages, isRead(), bookId);
-};
+  //Adds the input book to the library
+  const addBookToLibrary = () => {
+    const newBook = getUserInput();
+    library.push(newBook);
+    bookId++;
+    removeInputCard();
+    showBookCard();
+  };
 
-//Adds the input book to the library
-const addBookToLibrary = () => {
-  const newBook = getUserInput();
-  library.push(newBook);
-  bookId++;
-  removeInputCard();
-  showBookCard();
-};
+  //Adds a input card for the user to write info
+  const createInputCard = () => {
+    const inputCard = document.querySelector('#input-card');
+    inputCard.classList.remove('card-inactive')
+    inputCard.classList.add('card-active');
+    bookGrid.classList.add('grid-blurred');
+    resetInput();
+    toggleSwitchLabel();
 
-const showBookCard = () => {
-  resetBookGrid();
-  library.forEach(book => {
-    createBookCard(book);
-  });
-};
+    return {
+      inputCard
+    }
+  };
 
-const createBookCard = (book) => {
-  const card = document.createElement('div');
-  card.classList.add("card");
-  card.setAttribute("data-id", bookId);
+  const resetInput = () => {
+    const inputForm = document.querySelector('.form');
+    inputForm.reset();
+  };
 
-  const title = document.createElement('p');
-  const author = document.createElement('p');
-  const pages = document.createElement('p');
-  const readBtn = document.createElement('button');
-  const removeBtn = document.createElement('button');
+  const toggleSwitchLabel = () => {
 
-  //Content 
-  title.textContent = book.name;
-  author.textContent = book.author;
-  pages.textContent = `${book.pages} pages`;
-  !book.read ? read = "not read" : read = "read";
-  read.textContent = read;
-  readBtn.textContent = "READ";
-  removeBtn.textContent = "REMOVE";
+    const readSwitch = document.querySelector('#switch-toggle');
+    const readLabel = document.querySelector('.read-label');
+    const mainLabel = document.querySelector('.switch');
+    const switchContainer = document.querySelector('.switch-container');
 
-  //Style
-  title.style.color = "#298FFF";
-  readBtn.classList.add("action-read");
-  removeBtn.classList.add("action-remove");
+    if (readSwitch.checked === true) {
+      readLabel.textContent = "READ";
 
-  card.append(title, author, pages, read, readBtn, removeBtn);
-  bookGrid.appendChild(card);
+    } else if (!readSwitch.checked) {
+      readLabel.textContent = "NOT READ"
+    }
+    switchContainer.insertBefore(readLabel, mainLabel);
+    readSwitch.addEventListener('click', toggleSwitchLabel);
 
-  removeBtn.addEventListener('click', () => {
-    removeBookToLibrary(book.id)
-  });
-};
+    return {
+      readSwitch
+    }
+  };
 
-const removeBookToLibrary = (removeBookId) => {
+  const removeInputCard = () => {
+    const {
+      inputCard
+    } = createInputCard();
+    inputCard.classList.add('card-inactive');
+    inputCard.classList.remove('card-active');
+    bookGrid.classList.remove('grid-blurred');
+  };
 
-  removeBookId = parseInt(removeBookId);
-  
-  let index = library.findIndex((book) => book.id == removeBookId);
-      console.log(index);
-      library.splice(index, 1);
+  const getUserInput = () => {
+    const bookTitle = document.querySelector('#book-name').value;
+    const bookAuthor = document.querySelector('#book-author').value;
+    const bookPages = document.querySelector('#book-pages').value;
 
-  const clickedButton = document.querySelector('.action-remove');
-  const cardRemove = clickedButton.parentNode;
-  cardRemove.remove()
-  showBookCard()
-};
+    const {
+      readSwitch
+    } = toggleSwitchLabel();
+    //Saves the "read" status of a book entry
+    const isRead = readSwitch.onchange = () => {
+      let switchValue = readSwitch.checked;
+      return switchValue;
+    };
 
-const validateinput = () => {
+    return new Book(bookTitle, bookAuthor, bookPages, isRead(), bookId);
+  };
 
-  var inputbox = document.getElementById("book-pages");
+  const showBookCard = () => {
+    resetBookGrid();
+    library.forEach(book => {
+      createBookCard(book);
+    });
+  };
 
-  if (isNaN(parseFloat(inputbox.value))) {
-    inputbox.style.border = "3px solid #ff204c";
-  } else {
-    inputbox.style.border = "3px solid #1db75a";
+  const createBookCard = (book) => {
+    const card = document.createElement('div');
+    card.classList.add("card");
+    card.setAttribute("data-id", bookId);
+    
+    const title = document.createElement('p');
+    const author = document.createElement('p');
+    const pages = document.createElement('p');
+    const readStatus = document.createElement('p');
+    const readBtn = document.createElement('button');
+    const removeBtn = document.createElement('button');
+
+    //Content 
+    title.textContent = book.name;
+    author.textContent = book.author;
+    pages.textContent = `${book.pages} pages`;
+    !book.read ? readStatus.textContent = "not read" : readStatus.textContent = "read";
+    readBtn.textContent = "READ";
+    removeBtn.textContent = "REMOVE";
+
+    //Style
+    title.style.color = "#298FFF";
+    readBtn.classList.add("action-read");
+    removeBtn.classList.add("action-remove");
+    readStatus.classList.add("read-status")
+
+    card.append(title, author, pages, readStatus, readBtn, removeBtn);
+    bookGrid.appendChild(card);
+
+    readBtn.addEventListener('click', () => {
+      toggleRead(book.id)
+    })
+    removeBtn.addEventListener('click', () => {
+      removeBookFromLibrary(book.id)
+    });
+  };
+
+  const removeBookFromLibrary = (removeBookId) => {
+
+    removeBookId = parseInt(removeBookId);
+
+    let index = library.findIndex((book) => book.id == removeBookId);
+    library.splice(index, 1);
+
+    const clickedButton = document.querySelector('.action-remove');
+    const cardRemove = clickedButton.parentNode;
+    cardRemove.remove()
+    showBookCard()
+  };
+
+  const toggleRead = (readBookId) => {
+    readBookId = parseInt(readBookId);
+    const readStatus = document.querySelector('.read-status')
+    const readBtn = document.querySelector('.action-read')
+    library.findIndex((book) => {
+      if (book.id == readBookId) {
+        
+        book.read = !book.read
+
+        if (book.read) {
+          readStatus.textContent = "read"
+          readBtn.textContent = "not read"
+        } else {
+          readStatus.textContent = "not read"
+          readBtn.textContent = "read"
+        }
+      };
+    });
   }
-}
 
-const resetBookGrid = () => {
-  bookGrid.innerHTML = '';
-}
+  const resetBookGrid = () => {
+    bookGrid.innerHTML = '';
+  }
 
-//Event Listeners
-addButton.addEventListener('click', createInputCard);
-submitButton.addEventListener('click', addBookToLibrary);
+  //Event Listeners
+  addButton.addEventListener('click', createInputCard);
+  submitButton.addEventListener('click', addBookToLibrary);
+
+})();
